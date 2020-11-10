8109b657db8c2534106b6723f47101733a6bad36
Elvira Semenova <shalopayka@gmail.com>
2020-11-10 23:28:28 +0300

    files changed

--- src/main/java/ru/netology/bonus/Main.java
+++ src/main/java/ru/netology/bonus/Main.java
@@ -1,5 +1,6 @@
 package ru.netology.bonus;
 
 public class Main {
-    public static void main(String[] args) { }
+    public static void main(String[] args) {
+    }
 }
--- src/test/java/ru/netology/bonus/BonusServiceTest.java
+++ src/test/java/ru/netology/bonus/BonusServiceTest.java
@@ -1,8 +1,8 @@
-
 package ru.netology.bonus;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BonusServiceTest {
